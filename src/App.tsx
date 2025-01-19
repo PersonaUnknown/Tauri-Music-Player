0d7dcf2efcd1622697cacd930ed9785c8c540d11
NavelDum <69654548+PersonaUnknown@users.noreply.github.com>
2025-01-19 13:55:08 -0800

    chore: adding react-router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-	return <main className="container" />;
+	return (
+		<main className="container">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<div />} />
+				</Routes>
+			</BrowserRouter>
+		</main>
+	);
 }
 
 export default App;
